6978877f0d34dfa4c9631e2d007a118ff608af9c
Yossef Mendelssohn <ymendel@pobox.com>
2011-12-18 21:03:11 -0500

    Making the spoiler revalation even better.
    Now the link only appears for a list that actually has spoilers.
    And clicking it only reveals the spoilers underneath.

--- tvtropes.org.js
+++ tvtropes.org.js
@@ -1,5 +1,5 @@
-$("div#wikitext > ul").before("<a id='show-spoilers'>(reveal spoilers)</a>")
-$("a#show-spoilers").click(function(e) {
-    $("span.spoiler").removeClass("spoiler");
+$("ul:has(span.spoiler)").prepend("<a class='show-spoilers'>(reveal spoilers)</a>");
+$("a.show-spoilers").click(function(e) {
+    $(this).parent("ul").find("span.spoiler").removeClass("spoiler");
     e.preventDefault();
 });
