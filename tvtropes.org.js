3ad8f13f24150de4414d0bd2e38921c86f92e599
Yossef Mendelssohn <ymendel@pobox.com>
2011-12-18 20:28:40 -0500

    Putting spoiler-reveal link in right place
    It should be outside the list, says I.

--- tvtropes.org.js
+++ tvtropes.org.js
@@ -1,4 +1,4 @@
-$("div#wikitext > ul").prepend("<a id='show-spoilers'>(reveal spoilers)</a>")
+$("div#wikitext > ul").before("<a id='show-spoilers'>(reveal spoilers)</a>")
 $("a#show-spoilers").click(function(e) {
     $("span.spoiler").removeClass("spoiler");
     e.preventDefault();
