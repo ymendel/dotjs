654cb471e1fc27f4feb988c6643cbbc780cfdcb9
Yossef Mendelssohn <ymendel@pobox.com>
2011-12-25 01:45:53 -0500

    Cleaning up a post-reveal TV Tropes page.
    If there aren't spoilers to reveal, don't need a link to reveal them.

--- tvtropes.org.js
+++ tvtropes.org.js
@@ -1,5 +1,6 @@
 $("ul:has(span.spoiler)").prepend("<a class='show-spoilers'>(reveal spoilers)</a>");
 $("a.show-spoilers").click(function(e) {
     $(this).parent("ul").find("span.spoiler").removeClass("spoiler");
+    $("ul:not(:has(span.spoiler))").children("a.show-spoilers").remove();
     e.preventDefault();
 });
