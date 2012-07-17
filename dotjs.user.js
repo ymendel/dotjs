de80fb29aad76045e021f6dfb665b101ec313522
Yossef Mendelssohn <ymendel@pobox.com>
2012-07-17 02:30:32 -0400

    Trying to add dotjs for Fluid(ium)
    See https://github.com/sj26/dotjs-fluid
    
    It doesn't seem to work. Yet.

---
+++ dotjs.user.js
@@ -0,0 +1,43 @@
+// ==UserScript==
+// @name        dotjs
+// @namespace   https://github.com/sj26/dotjs-fluid
+// @description Run dotjs scripts via userscript for fluid apps
+// @include     *
+// @author      Samuel Cochran
+// @author      Corey Innis
+// ==/UserScript==
+
+(function () {
+  function getScript(uri, callback) {
+    var script = document.createElement('script');
+    var head   = document.head || document.getElementsByTagName( "head" )[0] || document.documentElement;
+
+    script.async  = 'async';
+    script.src    = uri;
+    script.onload = function onload() {
+      jQuery.noConflict();
+      callback();
+    };
+
+    head.insertBefore(script, head.firstChild);
+  }
+
+  function dotjs($) {
+    $.ajax({
+      url: 'http://localhost:3131/' + window.location.host.replace('www.','') + '.js',
+      dataType: 'text',
+      success: function(d){
+        $(function(){ eval(d); });
+      },
+      error: function(){
+        console.log('no dotjs server found at localhost:3131');
+      }
+    })
+  }
+
+  if (!window.jQuery) {
+    getScript('http://localhost:3131/jquery.js', function() {
+      dotjs(jQuery);
+    });
+  }
+})();
